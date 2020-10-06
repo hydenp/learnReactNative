8f2a1eaa2c566726995b17f6a660d46477dd401f
hydenpolikoff <hydenpolikoff@gmail.com>
2020-10-06 01:44:31 -0600

    main page experimentation

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+
+   // create the header
+   function header() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.headerTitle}>What's Sko'n on?</Text>
+        </View>
+      </View>
+    );
+  }
+
   return (
+
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* make a new view for the header of our app*/}
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>What's Sko'n on?</Text>
+      </View>
+
+      <Text>How does this fucking... does it still?</Text>
+
+      {/* make a view for the foot */}
+      <View style={styles.footer}>
+        <Text> Can we make a footer!</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Image style={styles.footerImg} source={require('./assets/court.png')} />
+          <Image style={styles.footerImg} source={require('./assets/profile.png')} />
+        </View>
+      </View>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'flex-start',
   },
+  header: {
+    width: '100%',
+    height: 80,
+    backgroundColor: '#B2B2B2',
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  headerTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  footer: {
+    
+    width: '100%',
+    height: 80,
+    backgroundColor: '#B2B2B2',
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  footerImg: {
+    marginHorizontal: 60,
+    marginBottom: 10,
+    height: 50,
+    width: 70,
+  }
 });
